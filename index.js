def96694089b86b4b3914a59587777d6da169610
Daniel Gomez Calderon <danielgomezcalderon@MacBook-Air-de-Daniel.local>
2020-01-02 20:57:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function clickButton() {
+    return alert('hey you clicked me naughty boy');
+}
+
+function deleteElement() {
+    const a = document;
+    return document.getElementById("produc1").style.display = "none";
+}
